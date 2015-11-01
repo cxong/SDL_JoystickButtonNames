364bac0d4fd583d88805b9e3672f56267552ebff
Cong <congusbongus@gmail.com>
2015-11-01 13:15:21 +1100

    Default names and colors for Xbox 360

--- SDL_joystickbuttonnames.c
+++ SDL_joystickbuttonnames.c
@@ -26,8 +26,18 @@
 
 #include <stdio.h>
 
+#include <SDL_pixels.h>
+
+
+typedef struct
+{
+	Uint8 r, g, b;
+} Color;
 
 const char *err = NULL;
+
+static const char *DefaultButtonName(SDL_GameControllerButton button);
+static SDL_Color DefaultButtonColor(SDL_GameControllerButton button);
 
 int SDLJBN_GetButtonNameAndColor(SDL_Joystick *joystick,
                                  SDL_GameControllerButton button,
@@ -50,21 +60,82 @@ int SDLJBN_GetButtonNameAndColor(SDL_Joystick *joystick,
 
 	if (s)
 	{
-		strcpy(s, SDL_GameControllerGetStringForButton(button));
+		// Use our own defaults for the button names
+		strcpy(s, DefaultButtonName(button));
 	}
+	const SDL_Color defaultColor = DefaultButtonColor(button);
 	if (r)
 	{
-		*r = 128;
+		*r = defaultColor.r;
 	}
 	if (g)
 	{
-		*g = 128;
+		*g = defaultColor.g;
 	}
 	if (b)
 	{
-		*b = 128;
+		*b = defaultColor.b;
 	}
 	return 0;
+}
+
+static const char *DefaultButtonName(SDL_GameControllerButton button)
+{
+	switch (button)
+	{
+	case SDL_CONTROLLER_BUTTON_A: return "A";
+	case SDL_CONTROLLER_BUTTON_B: return "B";
+	case SDL_CONTROLLER_BUTTON_X: return "X";
+	case SDL_CONTROLLER_BUTTON_Y: return "Y";
+	case SDL_CONTROLLER_BUTTON_BACK: return "Back";
+	case SDL_CONTROLLER_BUTTON_GUIDE: return "Guide";
+	case SDL_CONTROLLER_BUTTON_START: return "Start";
+	case SDL_CONTROLLER_BUTTON_LEFTSTICK: return "Left Stick";
+	case SDL_CONTROLLER_BUTTON_RIGHTSTICK: return "Right Stick";
+	case SDL_CONTROLLER_BUTTON_LEFTSHOULDER: return "LB";
+	case SDL_CONTROLLER_BUTTON_RIGHTSHOULDER: return "RB";
+	case SDL_CONTROLLER_BUTTON_DPAD_UP: return "D-pad Up";
+	case SDL_CONTROLLER_BUTTON_DPAD_DOWN: return "D-pad Down";
+	case SDL_CONTROLLER_BUTTON_DPAD_LEFT: return "D-pad Left";
+	case SDL_CONTROLLER_BUTTON_DPAD_RIGHT: return "D-pad Right";
+	default: return "";
+	}
+}
+
+static SDL_Color NewColor(Uint8 r, Uint8 g, Uint8 b);
+
+static SDL_Color DefaultButtonColor(SDL_GameControllerButton button)
+{
+	// Default colors for Xbox 360 controller
+	switch (button)
+	{
+	case SDL_CONTROLLER_BUTTON_A: return NewColor(96, 160, 0);
+	case SDL_CONTROLLER_BUTTON_B: return NewColor(240, 0, 0);
+	case SDL_CONTROLLER_BUTTON_X: return NewColor(0, 96, 208);
+	case SDL_CONTROLLER_BUTTON_Y: return NewColor(255, 160, 0);
+	case SDL_CONTROLLER_BUTTON_BACK: return NewColor(224, 224, 224);
+	case SDL_CONTROLLER_BUTTON_GUIDE: return NewColor(128, 176, 0);
+	case SDL_CONTROLLER_BUTTON_START: return NewColor(224, 224, 224);
+	case SDL_CONTROLLER_BUTTON_LEFTSTICK: return NewColor(96, 128, 128);
+	case SDL_CONTROLLER_BUTTON_RIGHTSTICK: return NewColor(96, 128, 128);
+	case SDL_CONTROLLER_BUTTON_LEFTSHOULDER: return NewColor(224, 224, 224);
+	case SDL_CONTROLLER_BUTTON_RIGHTSHOULDER: return NewColor(224, 224, 224);
+	case SDL_CONTROLLER_BUTTON_DPAD_UP: return NewColor(96, 128, 128);
+	case SDL_CONTROLLER_BUTTON_DPAD_DOWN: return NewColor(96, 128, 128);
+	case SDL_CONTROLLER_BUTTON_DPAD_LEFT: return NewColor(96, 128, 128);
+	case SDL_CONTROLLER_BUTTON_DPAD_RIGHT: return NewColor(96, 128, 128);
+	default: return NewColor(0, 0, 0);
+	}
+}
+
+static SDL_Color NewColor(Uint8 r, Uint8 g, Uint8 b)
+{
+	SDL_Color c;
+	c.r = r;
+	c.g = g;
+	c.b = b;
+	c.a = 255;
+	return c;
 }
 
 const char *SDLJBN_GetError(void)
