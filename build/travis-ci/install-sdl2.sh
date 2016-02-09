80dd1c04c44fc0c9d60f15db488efdc22ea1fb6a
Cong <congusbongus@gmail.com>
2016-02-09 23:36:57 +1100

    Revert to sudo travis build

--- build/travis-ci/install-sdl2.sh
+++ build/travis-ci/install-sdl2.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 set -e
-export SDL_PREFIX=$HOME/sdl2
-# check to see if sdl2 folder is empty
-if [ ! -d "$SDL_PREFIX" ]; then
-  curl -O http://www.libsdl.org/release/SDL2-2.0.3.tar.gz
-  tar -xzvf SDL2-2.0.3.tar.gz
-  (cd SDL2-2.0.3 && ./configure --prefix=$SDL_PREFIX && make && make install)
-else
-  echo "Using cached sdl2 directory."
-fi
+curl -O http://www.libsdl.org/release/SDL2-2.0.4.tar.gz
+tar -xzvf SDL2-2.0.4.tar.gz
+(cd SDL2-2.0.4 && ./configure && make && sudo make install)
